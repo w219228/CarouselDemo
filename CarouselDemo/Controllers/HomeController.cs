e62bd07b600e91e635ba737f73890b718242255c
willy <w219228@gmail.com>
2023-02-16 13:50:48 +0800

    加入專案檔案。

---
+++ CarouselDemo/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using CarouselDemo.DTO;
+using CarouselDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CarouselDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        
+        CarouselDTO[] CarouselDTOs=
+        {
+            new CarouselDTO
+            {
+                Caption="item1",
+                Src="https://picsum.photos/1024/480/?image=1"
+            },
+            new CarouselDTO
+            {
+                Caption="item2",
+                Src="https://picsum.photos/1024/480/?image=2"
+            },
+            new CarouselDTO
+            {
+                Caption="item3",
+                Src="https://picsum.photos/1024/480/?image=3"
+            }
+        };
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Carousel()
+        {
+            return View();
+        }
+        public IActionResult GetCarousels() 
+        {
+            return Json(CarouselDTOs);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CarouselDemo/DTO/CarouselDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace CarouselDemo.DTO
+{
+    public class CarouselDTO
+    {
+        public string? Caption { get; set; }
+        public string? Src { get; set; }
+    }
+}
